9f2c1f953c25e55d825fac425765001c0c04b79e
arda ırmak <132782431+Sperrex55@users.noreply.github.com>
2025-09-01 16:56:33 +0300

    Update app.py

--- tamirat-randevu-sistemi/app.py
+++ tamirat-randevu-sistemi/app.py
@@ -1683,15 +1683,17 @@ def delete_technician(id):
     return redirect(url_for('manage_technicians'))
 
 
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    socketio.run(app, host="0.0.0.0", port=port, debug=True)
+
+#if __name__ == '__main__':
+ #   with app.app_context():
+  #      db.create_all()
+   # socketio.run(app, debug=True)
 
 
-if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-    socketio.run(app, debug=True)
 
-#import os
-#if __name__ == "__main__":
-#   port = int(os.environ.get("PORT", 5000))
-#    socketio.run(app, host="0.0.0.0", port=port, debug=True)
 
